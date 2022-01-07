382b153ea8501859cade0ac7ae48fa1ee0ac6bed
NataliaBovarines <82040127+NataliaBovarines@users.noreply.github.com>
2022-01-07 18:25:25 -0300

    agenda electronica

---
+++ src/Lógica/Main.java
@@ -0,0 +1,18 @@
+
+package Lógica;
+
+import IGU.Principal;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+      Principal principal = new Principal() ; 
+      principal.setVisible(true);  
+      principal.setLocationRelativeTo(null); 
+        
+    }
+    
+}
